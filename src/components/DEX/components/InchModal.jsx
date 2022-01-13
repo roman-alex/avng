433ed6dc5784ca6cbfd0c6608ffa8f344ef75f31
RomanAlieksieiev <roman.alieksieiev@avenga.com>
2022-01-13 18:28:32 +0200

    search coins input 1inch

--- src/components/DEX/components/InchModal.jsx
+++ src/components/DEX/components/InchModal.jsx
@@ -1,13 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
+import Search from "antd/es/input/Search";
 
 function InchModal({ open, onClose, setToken, tokenList }) {
+  const [tokenSearch, setTokenSearch] = useState({});
+
   if (!open) return null;
+
+  const onSearch = value => {
+    let newTokenList = {};
+
+    if (value.length === 42 && tokenList[value].address === value) {
+      newTokenList[value] = tokenList[value];
+
+    } else if (value.length > 1) {
+      for (const tokenKey in tokenList) {
+        if (tokenList[tokenKey].symbol.includes(value) || tokenList[tokenKey].name.includes(value)) {
+          newTokenList[tokenKey] = tokenList[tokenKey];
+        }
+      }
+
+    } else {
+      newTokenList = {};
+    }
+
+    setTokenSearch(newTokenList);
+  }
 
   return (
     <div style={{ overflow: "auto", height: "500px" }}>
-      {!tokenList
+      <Search placeholder="Search token" onSearch={onSearch} style={{ padding: "10px 20px" }} />
+      {!tokenSearch
         ? null
-        : Object.keys(tokenList).map((token, index) => (
+        : Object.keys(tokenSearch).map((token, index) => (
             <div
               style={{
                 padding: "5px 20px",
@@ -17,6 +41,7 @@ function InchModal({ open, onClose, setToken, tokenList }) {
               }}
               onClick={() => {
                 setToken(tokenList[token]);
+                setTokenSearch({})
                 onClose();
               }}
               key={index}
