9b463ae618eabba3ff787fc36d0c3d8dc622bebf
RomanAlieksieiev <roman.alieksieiev@avenga.com>
2022-01-13 15:57:43 +0200

    header navigation modify

--- src/components/MenuItems.jsx
+++ src/components/MenuItems.jsx
@@ -1,6 +1,7 @@
 import { useLocation } from "react-router";
 import { Menu } from "antd";
 import { NavLink } from "react-router-dom";
+const { SubMenu } = Menu;
 
 function MenuItems() {
   const { pathname } = useLocation();
@@ -19,29 +20,31 @@ function MenuItems() {
       defaultSelectedKeys={[pathname]}
     >
       <Menu.Item key="/quickstart">
-        <NavLink to="/quickstart">🚀 Quick Start</NavLink>
-      </Menu.Item>
-      <Menu.Item key="/wallet">
-        <NavLink to="/wallet">👛 Wallet</NavLink>
-      </Menu.Item>
-      <Menu.Item key="/1inch">
-        <NavLink to="/1inch">🏦 Dex</NavLink>
-      </Menu.Item>
-      <Menu.Item key="onramp">
-        <NavLink to="/onramp">💵 Fiat</NavLink>
+        <NavLink to="/quickstart">Quick Start</NavLink>
       </Menu.Item>
       <Menu.Item key="/erc20balance">
-        <NavLink to="/erc20balance">💰 Balances</NavLink>
+        <NavLink to="/erc20balance">Balances</NavLink>
       </Menu.Item>
       <Menu.Item key="/erc20transfers">
-        <NavLink to="/erc20transfers">💸 Transfers</NavLink>
+        <NavLink to="/erc20transfers">Transfers</NavLink>
       </Menu.Item>
-      <Menu.Item key="/nftBalance">
-        <NavLink to="/nftBalance">🖼 NFTs</NavLink>
-      </Menu.Item>
-      <Menu.Item key="/contract">
-        <NavLink to="/contract">📄 Contract</NavLink>
-      </Menu.Item>
+      <SubMenu key="SubMenu" title="Payment Methods">
+        <Menu.Item key="/wallet">
+          <NavLink to="/wallet">Wallet</NavLink>
+        </Menu.Item>
+        <Menu.Item key="/1inch">
+          <NavLink to="/1inch">Dex</NavLink>
+        </Menu.Item>
+        <Menu.Item key="onramp">
+          <NavLink to="/onramp">Fiat</NavLink>
+        </Menu.Item>
+      </SubMenu>
+      {/*<Menu.Item key="/nftBalance">*/}
+      {/*  <NavLink to="/nftBalance">🖼 NFTs</NavLink>*/}
+      {/*</Menu.Item>*/}
+      {/*<Menu.Item key="/contract">*/}
+      {/*  <NavLink to="/contract">📄 Contract</NavLink>*/}
+      {/*</Menu.Item>*/}
     </Menu>
   );
 }
